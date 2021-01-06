209defdcefe9e16b279eac6b7ed4c45ef3e80cc2
Pavel Halko <65878619+Pavel-2004@users.noreply.github.com>
2021-01-06 16:38:39 -0500

    Update staticfunction.php

--- staticfunction.php
+++ staticfunction.php
@@ -1,30 +1,49 @@
 <?php
+require 'vendor/autoload.php'; 
+use Carbon\Carbon; 
 class getDays {
-    public $curl;
-    public $responses;
-    public $test3;
-    function __construct(){
-        $this->curl = curl_init();
-        curl_setopt_array($this->curl,[
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => 'https://canada-holidays.ca/api/v1/provinces/ON',
-        CURLOPT_USERAGENT => 'API'
-        ]);
-        $this->responses = curl_exec($this->curl);
-        $this->responses = json_decode($this->responses, true);
-        $this->responses = $this->responses['province']['holidays'];
+    public static function check1($date, $newArray){
+        $bool = false;
         
-    }
-    public function getDate($date, $delivery){
-        foreach($this->responses as $response):
-            if ($response['date'] === $date){
-                $delivery += 1;
+        foreach($newArray as $response):
+            if ($date === $response['date']){
+                $bool = true;
             }
         endforeach;
-        return $delivery; 
+        return $bool; 
     }
-
+    public static function check2($date){
+        $bool2 = false; 
+        if ($date->dayOfWeek == 6 or $date->dayOfWeek === 0){
+            $bool2 = True;
+        }
+        return $bool2;
+    }
+    public static function getDate($date, $days){
+        $year = mb_substr($date, 0, 4);
+        $curl = curl_init();
+        curl_setopt_array($curl,[
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://canada-holidays.ca/api/v1/provinces/ON?year=' . $year,
+            CURLOPT_USERAGENT => 'API'
+        ]);
+        $responses = curl_exec($curl);
+        $responses = json_decode($responses, true);
+        $responses = $responses['province']['holidays'];
+        $final = new Carbon($date);
+        $i = 0; 
+        while($i !== $days):
+            if (getDays::check1($final->toDateString(), $responses)){
+                $final->addDay();
+            } elseif (getDays::check2($final)){
+                $final->addDay();
+            } else{
+                $final->addDay();
+                $i++; 
+            }
+        endwhile;
+        return $final->toDateString(); 
+    }
 }
-//test
-$test = new getDays();
-print_r($test->getDate('2021-01-01', 5));
+#test
+echo(getDays::getDate('2021-01-01', 3));
